18ae6900fa12a217105eae9842d01c201aeb9e8b
Avishek verma <79629526+AvishekVerma@users.noreply.github.com>
2021-12-28 23:44:12 +0530

    both tag added

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,11 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <h1 data-ns-test="project-name">Rendering MUltiple Components</h1>
+      <p data-ns-test="project-description">In  this project I am displying multiple HTML tag in react JSX</p>
+    
+    </div>
   )
 }
 
